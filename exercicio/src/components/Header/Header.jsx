8e17945a4eff0f061ff42f1a3f38e387e6cda26e
Geliel1 <85584428+Geliel1@users.noreply.github.com>
2021-11-21 13:43:53 -0300

    finalizando nav bar

---
+++ exercicio/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export function Header(){
+
+    return(
+        <header className='container-header'>
+
+            <nav className='nav-bar'>
+                <a href=".">Home</a>
+                <a href=".">Perfil</a>
+                <a href=".">Amigos</a>
+                <a href=".">Notificação</a>
+            </nav>
+
+        </header>
+    );
+}
